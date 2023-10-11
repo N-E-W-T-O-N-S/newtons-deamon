7dcfadd2b92262799bcd27b7290e9275cd0cd160
Kargisa <personal@kargisa.com>
2023-10-11 17:14:34 +0200

    Classes added

---
+++ Core/Physics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEWTONS.Core
+{
+    public class Physics
+    {
+        public List<KinematicBody> Bodies { get; set; } = new List<KinematicBody>();
+
+        
+    }
+}
---
+++ Core/SphereCollider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEWTONS.Core
+{
+    public class SphereCollider
+    {
+        public float Radius { get; set; }
+        public Vector3 Offset { get; set; }
+
+        public SphereCollider(float radius, Vector3 offset)
+        {
+            Radius = radius;
+            Offset = offset;
+        }
+    }
+}

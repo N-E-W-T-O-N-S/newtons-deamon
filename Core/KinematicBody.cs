576e863bf5045dcebe15058a8fe0acc149529d09
Kargisa <luca.wolf@outlook.com>
2023-10-18 16:12:06 +0200

    Garvity and Velocity implemented

--- Core/KinematicBody.cs
+++ Core/KinematicBody.cs
@@ -12,10 +12,16 @@ namespace NEWTONS.Core
         public Vector3 Velocity { get; set; }
         public Vector3 CenterOfMass { get; set; }
         public float Mass { get; set; }
+        public bool UseGravity { get; set; } = true;
+
+        public KinematicBody() 
+        { 
+            Physics.Bodies.Add(this);
+        }
 
         public void MoveToPosition(Vector3 newPosition)
         {
-            throw new NotImplementedException();
+            Position = newPosition;
         }
         public void MoveToRotation(Quaternion newRotation)
         {

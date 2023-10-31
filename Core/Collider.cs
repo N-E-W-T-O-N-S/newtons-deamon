b81d157211bc8c6ca175daffad48250fe810aaae
Kargisa <luca.wolf@outlook.com>
2023-10-31 20:02:29 +0100

    Cuboid Collisioon detection on x axis (no rotation)

--- Core/Collider.cs
+++ Core/Collider.cs
@@ -6,14 +6,16 @@ namespace NEWTONS.Core
 {
     public class Collider
     {
-        public Collider(KinematicBody? kinematicBody, Vector3 center, PrimitiveShape shape)
+        public Collider(KinematicBody kinematicBody, Vector3 center, PrimitiveShape shape)
         {
             KinematicBody = kinematicBody;
             Center = center;
             Shape = shape;
+            Physics.Collideres.Add(this);
         }
 
-        public KinematicBody? KinematicBody { get; set; }
+        //TODO: Needs to be notified when the kinematic body is removed from the scene
+        public KinematicBody KinematicBody { get; set; }
         public Vector3 Center { get; set; }
         public PrimitiveShape Shape { get; }
     }
---
+++ Core/KonvexCollider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEWTONS.Core
+{
+    public class KonvexCollider : Collider
+    {
+        public KonvexCollider(Vector3 scale, KinematicBody kinematicBody, Vector3 center, PrimitiveShape shape, Vector3[] points) : base(kinematicBody, center, shape)
+        {
+            Scale = scale;
+            Points = points;
+        }
+
+        public Vector3 Scale { get; set; }
+        public Vector3[] Points { get; set; }
+    }
+}

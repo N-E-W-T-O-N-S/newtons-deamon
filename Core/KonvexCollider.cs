c018f73886cfc5c2945a3b3faa19fecd32e79edb
Kargisa <personal@kargisa.com>
2023-10-31 20:07:22 +0100

    Cuboid Collisioon detection on x axis (no rotation)

---
+++ Core/KonvexCollider.cs
@@ -0,0 +1,14 @@
+﻿namespace NEWTONS.Core
+{
+    public class KonvexCollider : Collider
+    {
+        public KonvexCollider(Vector3 scale, Vector3[] points, KinematicBody kinematicBody, Vector3 center, PrimitiveShape shape) : base(kinematicBody, center, shape)
+        {
+            Scale = scale;
+            Points = points;
+        }
+
+        public Vector3 Scale { get; set; }
+        Vector3[] Points { get; set; }
+    }
+}

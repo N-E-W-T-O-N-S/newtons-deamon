239cbd30bd731b4ead2a11a0232791cdfd510846
Kargisa <personal@kargisa.com>
2023-10-31 20:02:48 +0100

    Revert "Cuboid Collisioon detection on x axis (no rotation)"
    This reverts commit b89f31319528e3fe7a51532ac66f4a7effe49ab7.

--- Core/Collider.cs
+++ Core/Collider.cs
@@ -6,16 +6,14 @@ namespace NEWTONS.Core
 {
     public class Collider
     {
-        public Collider(KinematicBody kinematicBody, Vector3 center, PrimitiveShape shape)
+        public Collider(KinematicBody? kinematicBody, Vector3 center, PrimitiveShape shape)
         {
             KinematicBody = kinematicBody;
             Center = center;
             Shape = shape;
-            Physics.Collideres.Add(this);
         }
 
-        //TODO: Needs to be notified when the kinematic body is removed from the scene
-        public KinematicBody KinematicBody { get; set; }
+        public KinematicBody? KinematicBody { get; set; }
         public Vector3 Center { get; set; }
         public PrimitiveShape Shape { get; }
     }

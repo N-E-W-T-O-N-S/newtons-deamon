b89f31319528e3fe7a51532ac66f4a7effe49ab7
Kargisa <personal@kargisa.com>
2023-10-31 20:02:29 +0100

    Cuboid Collisioon detection on x axis (no rotation)

--- Core/CuboidCollider.cs
+++ Core/CuboidCollider.cs
@@ -4,13 +4,36 @@ using System.Text;
 
 namespace NEWTONS.Core
 {
-    public class CuboidCollider : Collider
+    public class CuboidCollider : KonvexCollider
     {
-        public CuboidCollider(Vector3 scale, KinematicBody? kinematicBody, Vector3 center, PrimitiveShape shape) : base(kinematicBody, center, shape)
+        private static readonly Vector3[] points = new Vector3[8]
         {
-            Scale = scale;
+            new Vector3(-0.5f, 0.5f, 0.5f),
+            new Vector3(0.5f, 0.5f, 0.5f),
+            new Vector3(-0.5f, 0.5f, -0.5f),
+            new Vector3(0.5f, 0.5f, -0.5f),
+            new Vector3(-0.5f, -0.5f, 0.5f),
+            new Vector3(0.5f, -0.5f, 0.5f),
+            new Vector3(-0.5f, -0.5f, -0.5f),
+            new Vector3(0.5f, -0.5f, -0.5f)
+        };
+
+        public CuboidCollider(Vector3 scale, KinematicBody kinematicBody, Vector3 center) : base(scale, kinematicBody, center, PrimitiveShape.Cube, points)
+        {
+
         }
 
-        public Vector3 Scale { get; set; }
+        public bool IsColliding(KonvexCollider other)
+        {
+            float length = Mathf.Abs((other.Center.x + other.KinematicBody.Position.x) - (Center.x + KinematicBody.Position.x));
+            //TODO: scale != width, height and length for all konvex shapes
+            float half_w_k1 = Scale.x / 2;
+            float half_w_k2 = other.Scale.x / 2;
+            float gap = length - (half_w_k1 + half_w_k2);
+
+            if (gap < 0)
+                return true;
+            return false;
+        }
     }
 }

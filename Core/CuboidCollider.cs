03f646e62d1f4a46fd73e181f6bedba9e5457093
Kargisa <personal@kargisa.com>
2023-11-06 00:03:23 +0100

    cuboid collision detection on all 3 axis

--- Core/CuboidCollider.cs
+++ Core/CuboidCollider.cs
@@ -25,13 +25,24 @@ namespace NEWTONS.Core
 
         public bool IsColliding(KonvexCollider other)
         {
-            float length = Mathf.Abs((other.Center.x + other.KinematicBody.Position.x) - (Center.x + KinematicBody.Position.x));
+            float lengthX = Mathf.Abs((other.Center.x + other.KinematicBody.Position.x) - (Center.x + KinematicBody.Position.x));
+            float lengthY = Mathf.Abs((other.Center.y + other.KinematicBody.Position.y) - (Center.y + KinematicBody.Position.y));
+            float lengthZ = Mathf.Abs((other.Center.z + other.KinematicBody.Position.z) - (Center.z + KinematicBody.Position.z));
+
             //TODO: scale != width, height and length for all konvex shapes
             float half_w_k1 = Scale.x / 2;
             float half_w_k2 = other.Scale.x / 2;
-            float gap = length - (half_w_k1 + half_w_k2);
+            float gapX = lengthX - (half_w_k1 + half_w_k2);
+            
+            float half_h_k1 = Scale.y / 2;
+            float half_h_k2 = other.Scale.y / 2;
+            float gapY = lengthY - (half_h_k1 + half_h_k2);
 
-            if (gap < 0)
+            float half_d_k1 = Scale.z / 2;
+            float half_d_k2 = other.Scale.z / 2;
+            float gapZ = lengthZ - (half_d_k1 + half_d_k2);
+
+            if (gapX < 0 && gapY < 0 && gapZ < 0)
                 return true;
             return false;
         }
---
+++ Core/IKinematicBodyRefference.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEWTONS.Core
+{
+    public interface IKinematicBodyRefference
+    {
+        public void Dispose();
+    }
+}

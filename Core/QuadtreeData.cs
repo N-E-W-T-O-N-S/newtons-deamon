c0b228b786cd613960fb353fa86bd1c69fc0603b
Kargisa <luca.wolf@outlook.com>
2023-12-09 20:16:20 +0100

    Quadtree

---
+++ Core/QuadtreeData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEWTONS.Core
+{
+    public struct QuadtreeData<T>
+    {
+        public Vector3 Position { get; set; }
+        public T Data { get; set; }
+
+        public QuadtreeData(Vector3 position, T data)
+        {
+            Position = position;
+            Data = data;
+        }
+    }
+}

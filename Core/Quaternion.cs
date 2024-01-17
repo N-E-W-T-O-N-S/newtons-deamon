8a02529ae23796397c89ccf76faa05f8eb925b4b
tobi <tischler.dev@gmail.com>
2024-01-17 14:44:40 +0100

    toEulerAngle toQuaternion

--- Core/Quaternion.cs
+++ Core/Quaternion.cs
@@ -1,4 +1,6 @@
-﻿namespace NEWTONS.Core
+﻿using System;
+
+namespace NEWTONS.Core
 {
     [System.Serializable]
     public struct Quaternion
@@ -60,7 +62,7 @@
         {
             float mag = Magnitude(q);
 
-            return new Quaternion(-1 * q.x / mag, -1 * q.y / mag, -1 *  q.z / mag, q.w / mag);
+            return new Quaternion(1 * q.x / mag, 1 * q.y / mag, 1 *  q.z / mag, q.w / mag);
         }
 
         public static Quaternion Conjugate(Quaternion q)
@@ -88,6 +90,46 @@
             Quaternion resultQuat = q * vecQuat * Conjugate(q);
 
             return new Vector3(resultQuat.x, resultQuat.y, resultQuat.z);
+        }
+
+        public static Vector3 ToEulerAngle(Quaternion q)
+        {
+            Vector3 angles;
+            
+            // x-Axis
+            float sinr_cosp = 2 * (q.w * q.x + q.y * q.z);
+            float cosr_cosp = 1 - 2 * (q.x * q.x + q.y * q.y);
+            angles.x = Mathf.Atan2(sinr_cosp, cosr_cosp);
+            
+            // y-Axis
+            float sinp = Mathf.Sqrt(1 + 2 * (q.w * q.y - q.x * q.z));
+            float cosp = Mathf.Sqrt(1 - 2 * (q.w * q.y - q.x * q.z));
+            angles.y = 2 * Mathf.Atan2(sinp, cosp) - Mathf.PI / 2;
+            
+            // z-Axis
+            float siny_cosp = 2 * (q.w * q.z + q.x * q.y);
+            float cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z);
+            angles.z = Mathf.Atan2(siny_cosp, cosy_cosp);
+
+            return angles;
+        }
+
+        public static Quaternion ToQuaternion(Vector3 v)
+        {
+            float cr = Mathf.Cos(v.x * 0.5f);
+            float sr = Mathf.Sin(v.x * 0.5f);
+            float cp = Mathf.Cos(v.y * 0.5f);
+            float sp = Mathf.Sin(v.y * 0.5f);
+            float cy = Mathf.Cos(v.z * 0.5f);
+            float sy = Mathf.Sin(v.z * 0.5f);
+
+            Quaternion q;
+            q.w = cr * cp * cy + sr * sp * sy;
+            q.x = sr * cp * cy - cr * sp * sy;
+            q.y = cr * sp * cy + sr * cp * sy;
+            q.z = cr * cp * sy - sr * sp * cy;
+
+            return q;
         }
 
         public override string ToString()

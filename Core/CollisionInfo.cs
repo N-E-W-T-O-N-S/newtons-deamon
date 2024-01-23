514c3e97879f0bd862609dee88b39a4721e17f31
Kargisa <luca.wolf@outlook.com>
2024-01-23 22:37:29 +0100

    Fixed collision response scale bug

---
+++ Core/CollisionInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEWTONS.Core
+{
+    public struct CollisionInfo
+    {
+        public bool didCollide;
+        public Vector3 Normal;
+    }
+}
--- Core/CuboidCollider.cs
+++ Core/CuboidCollider.cs
@@ -52,70 +52,13 @@ namespace NEWTONS.Core
 
         }
 
-        public override bool IsColliding(KonvexCollider other)
+        public override CollisionInfo IsColliding(KonvexCollider other)
         {
-            bool didCollide = base.IsColliding(other);
-            if (!didCollide)
-                return false;
-            //bool colliding = false;
-            //float lengthX = Mathf.Abs((other.Center.x + other.Body.Position.x) - (Center.x + Body.Position.x));
-            //float lengthY = Mathf.Abs((other.Center.y + other.Body.Position.y) - (Center.y + Body.Position.y));
-            //float lengthZ = Mathf.Abs((other.Center.z + other.Body.Position.z) - (Center.z + Body.Position.z));
+            CollisionInfo info = base.IsColliding(other);
 
-            ////TODO: scale != width, height and length for all konvex shapes
-            //float half_w_k1 = GlobalScales.x / 2;
-            //float half_w_k2 = other.GlobalScales.x / 2;
-            //float gapX = lengthX - (half_w_k1 + half_w_k2);
+            CollisionResponse(this, other, info);
 
-            //float half_h_k1 = GlobalScales.y / 2;
-            //float half_h_k2 = other.GlobalScales.y / 2;
-            //float gapY = lengthY - (half_h_k1 + half_h_k2);
-
-            //float half_d_k1 = GlobalScales.z / 2;
-            //float half_d_k2 = other.GlobalScales.z / 2;
-            //float gapZ = lengthZ - (half_d_k1 + half_d_k2);
-
-            //if (gapX < 0 && gapY < 0 && gapZ < 0)
-            //{
-            //    if (Body.Velocity == Vector3.Zero || Body.IsStatic)
-            //        return true;
-
-            //    //TODO: Not working properly with different scales
-
-            //    colliding = true;
-            //    Vector3 dir = -Body.Velocity.Normalized;
-            //    Vector3 bodyDir = other.Body.Position - Body.Position;
-
-            //    float absX = Mathf.Abs(gapX);
-            //    float absY = Mathf.Abs(gapY);
-            //    float absZ = Mathf.Abs(gapZ);
-            //    float xDistSquare = bodyDir.x * bodyDir.x;
-            //    float yDistSquare = bodyDir.y * bodyDir.y;
-            //    float zDistSquare = bodyDir.z * bodyDir.z;
-
-            //    float gap = 0f;
-            //    if (((xDistSquare > yDistSquare && xDistSquare > zDistSquare) || xDistSquare == yDistSquare || (dir.y == 0 && dir.z == 0)) && dir.x != 0)
-            //    {
-            //        gap = absX / Mathf.Abs(dir.x);
-            //    }
-            //    else if (((yDistSquare > xDistSquare && yDistSquare > zDistSquare) || yDistSquare == zDistSquare || (dir.x == 0 && dir.z == 0)) && dir.y != 0)
-            //    {
-            //        gap = absY / Mathf.Abs(dir.y);
-            //    }
-            //    else if (dir.z != 0)
-            //    {
-            //        gap = absZ / Mathf.Abs(dir.z);
-            //    }
-
-            //Vector3 backDist = dir * gap;
-            //    //Body.Velocity = Vector3.Zero;
-            //Body.MoveToPosition(Body.Position + backDist);
-            CollisionResponse(this, other);
-
-
-            //}
-            //return colliding;
-            return didCollide;
+            return info;
         }
 
         public void AddToPhysicsEngine()
@@ -124,8 +67,11 @@ namespace NEWTONS.Core
                 Physics.Colliders.Add(this);
         }
 
-        public void CollisionResponse(KonvexCollider body, KonvexCollider other)
+        public void CollisionResponse(KonvexCollider body, KonvexCollider other, CollisionInfo info)
         {
+            if (!info.didCollide)
+                return;
+
             KinematicBody firstBody = body.Body;
             KinematicBody secondBody = other.Body;
 
@@ -138,7 +84,10 @@ namespace NEWTONS.Core
             float e = (body.Restitution + other.Restitution) / 2;
             float meff;
 
-            Vector3 n = (pos2 - pos1) / Vector3.Magnitude(pos2 - pos1);
+            //Vector3 n = (pos2 - pos1).Normalized;
+            Vector3 n = info.Normal;
+
+
             if (secondBody.IsStatic)
                 meff = m1;
             else if (firstBody.IsStatic)
